66be791928fba7589850b511e789723d407ab826
yannael <yannael@gmail.com>
2015-06-26 19:57:48 +0200

    Preprocessing compound

--- Web/global.R
+++ Web/global.R
@@ -1,9 +1,10 @@
 require("rvest")
 require("magrittr")
-
+require("RMySQL")
+require("rjson")
 
 load(file="phenotypes.Rdata")
-phenotypesAll<-rbind(phenotypesErasme,phenotypes1000Gen)
+phenotypesAll<-rbind(phenotypesULB,phenotypes1000Gen)
 
 username<-'yleborgn'
 
@@ -11,39 +12,69 @@ load("mySampleGroups.Rdata")
 
 load("myResults.Rdata")
 
+get41<-function(l) {l[[4]][[1]]}
+get42<-function(l) {l[[4]][[2]]}
 get4<-function(l) {l[[4]]}
 get1<-function(l) {l[[1]]}
 get2<-function(l) {l[[2]]}
 
 procRes<-function(results) {
   con <- dbConnect(RSQLite::SQLite(), "../groupsToComparePartial.db")
-  resAll<-list()
   if (results[[2]]=="singleVariant") {
     res<-list()
     res$name<-results[[1]]
     res$type<-"singleVariant"
     res$genotypes<-t(as.data.frame(sapply(results[[3]],get4)))
     res$locus<-sapply(results[[3]],get1)
-    i<-1
-    infSNPs<-NULL
-    for (i in 1:10) {
-      rs<-dbSendQuery(con,paste0("select * from dbSNPs where Locus='",res$locus[2,i],"' and reference='", 
-                               res$locus[3,i],"' and alternative='",res$locus[4,i],"'"))
-      infSNPs<-rbind(infSNPs,unique(fetch(rs, n=-1))[1,])
-    }
-    res$infoSNPs<-infSNPs
+    uniqueid<-apply(res$locus[2:4,],2,paste,collapse=":")
+    dbvariants<-dbReadTable(con,"dbvariants")
+    to.keep<-match(uniqueid,dbvariants$uniqueid)
+    res$infovariants<-dbvariants[to.keep,]
+    
     res$scores<-sapply(results[[3]],get2)
-    resAll<-c(resAll,list(res))
+    
+    scoreSummary<-cbind(res$locus[2,],res$infovariants[,'reference'],res$infovariants[,'alternative'],res$infovariants[,'gene_symbol'],res$score)
+    colnames(scoreSummary)<-c("Locus","Reference", "Alternative","Gene symbol","Score")
+    res$scoreSummary<-scoreSummary
+    
+  }
+  if (results[[2]]=="pairVariantsMonogenic") {
+    res<-list()
+    res$name<-results[[1]]
+    res$type<-"pairVariantsMonogenic"
+    res$genotypes1<-t(as.data.frame(sapply(results[[3]],get41)))
+    res$genotypes2<-t(as.data.frame(sapply(results[[3]],get42)))
+    res$locus<-sapply(results[[3]],get1)
+    uniqueid1<-apply(res$locus[2:4,],2,paste,collapse=":")
+    dbvariants<-dbReadTable(con,"dbvariants")
+    to.keep<-match(uniqueid1,dbvariants$uniqueid)
+    res$infovariants1<-dbvariants[to.keep,]
+    uniqueid2<-apply(res$locus[5:7,],2,paste,collapse=":")
+    dbvariants<-dbReadTable(con,"dbvariants")
+    to.keep<-match(uniqueid2,dbvariants$uniqueid)
+    res$infovariants2<-dbvariants[to.keep,]
+    
+    res$scores<-sapply(results[[3]],get2)
+    
+    scoreSummary<-cbind(res$locus[2,],res$infovariants1[,'reference'],res$infovariants1[,'alternative'],res$locus[5,],res$infovariants2[,'reference'],res$infovariants2[,'alternative'],res$infovariants1[,'gene_symbol'],res$score)
+    colnames(scoreSummary)<-c("Locus1","Reference1", "Alternative1","Locus2","Reference2", "Alternative2","Gene symbol","Score")
+    res$scoreSummary<-scoreSummary
+    
   }
   dbDisconnect(con)
-  resAll
+  res
 }
 
 
 con <- dbConnect(RSQLite::SQLite(), "../groupsToComparePartial.db")
-rs<-dbSendQuery(con,"select * from dbSNPs" )
-dbSNPs<-fetch(rs, n=-1)
+rs<-dbSendQuery(con,"select * from dbvariants" )
+dbvariants<-fetch(rs, n=-1)
   
-results<-fromJSON("singVarDeNovo.txt")
+results<-fromJSON(file="singVarDeNovo2.txt")
+results1<-list(procRes(results))
+results<-fromJSON(file="pairVarCompound2.txt")
+results2<-list(procRes(results))
+results<-list(results1,results2)
+names(results)<-c("Trios_De_Novo","Trios_Compound_Heterozygous")
 
-results<-procRes(results)
+
