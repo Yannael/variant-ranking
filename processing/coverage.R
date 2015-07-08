a5793184f57ef402089648ccd77cd817a0b4277a
yannael <yannael@gmail.com>
2015-07-08 18:56:39 +0200

    Without locus data

--- processing/coverage.R
+++ processing/coverage.R
@@ -1,4 +1,6 @@
-library(RMySQL)
+require(RMySQL)
+require(reshape2)
+require(plyr)
 
 createCoverageDB<-function() {
   
@@ -90,7 +92,7 @@ retrieveRefEntriesChr<-function(chr,patho,control) {
   varMat
 }
 
-retrieveRefEntriesChr2<-function(chr,patho,control) {
+retrieveRefEntriesChr2<-function(patho,control) {
   
   patientID<-read.table('mappingZH_ISDBM_withFMC.txt',stringsAsFactors=F)[,1]
   
@@ -123,31 +125,20 @@ retrieveRefEntries<-function() {
   
   #Move patho and control groups to coverage DB
   congroups <- dbConnect(RSQLite::SQLite(), "groupsToComparePartial.db")
-  concoverage<- dbConnect(RSQLite::SQLite(), "coverage.db")
+  #concoverage<- dbConnect(RSQLite::SQLite(), "coverage.db")
   
   rs<-dbSendQuery(congroups,"select * from patho" )
   patho<-fetch(rs, n=-1)
   rs<-dbSendQuery(congroups,"select * from control" )
   control<-fetch(rs, n=-1)
   
-  #dbWriteTable(concoverage,"patho",patho,overwrite=T)
-  #dbWriteTable(concoverage,"control",control,overwrite=T)
+  #locusvariants<-data.frame(unique(patho[,c("Locus",'reference','alternative')]),stringsAsFactors=F)
+  #dbWriteTable(concoverage,"locus",locusvariants,overwrite=T)
   
-  locusvariants<-data.frame(unique(patho[,c("Locus",'reference','alternative')]),stringsAsFactors=F)
-  dbWriteTable(concoverage,"locus",locusvariants,overwrite=T)
-  
-  varMat<-NULL
-  
-  for (chr in c(1:22)) {
-    print(chr)
-    st<-system.time(varMat<-rbind(varMat,retrieveRefEntriesChr(paste0("chr",chr),patho,control)))
-    print(st)
-                   
-  }
+  st<-system.time(varMat<-rbind(varMat,retrieveRefEntriesChr2(paste0("chr",chr),patho,control)))
   
   rs<-dbSendQuery(congroups,"select * from dbvariants" )
   dbvariants<-fetch(rs, n=-1)
-  
   
   mapping_gene<-match(varMat$Locus,dbvariants$Locus)
   varMat<-cbind(geneid=dbvariants$gene_ensembl[mapping_gene],varMat)
@@ -165,63 +156,8 @@ retrieveRefEntries<-function() {
   i.keep<-match(id.keep,rownames(varMat))
   varMat<-varMat[i.keep,]
   
-  write.table(file="varMat2.txt",varMat,col.names=F,row.names=F,quote=F)
+  write.table(file="varMat.txt",varMat,col.names=F,row.names=F,quote=F)
   
 }
 
-dummy<-function() {
-  
-  save(file="../../snpsMat.Rdata",snpsMat)
-  
-  patient_data<-colnames(snpsMat)
-  patient_data<-sapply(patient_data,strsplit,'_')
-  patient_data<-unlist(patient_data,use.name=F)
-  patient_data<-matrix(patient_data,ncol(v),3,byrow=T)
-  patient_data<-patient_data[order(as.numeric(patient_data[,3]),patient_data[,2]),]
-  snpsMat<-snpsMat[,apply(patient_data,1,paste0,"",collapse="_")]
-              
-  write.table(file="../../snpsMat.txt",snpsMat,col.names=F,quote=F)
-  write.table(file="../../pedigree.txt",patient_data,col.names=F,row.names=F,quote=F)
-  
-  hadoop fs -put snpsMat.txt /user/yleborgn/MR/snpsMat.txt
-  hadoop fs -put pedigree.txt /user/yleborgn/MR/pedigree.txt
-  
-}
 
-dummy<-function() {
-  
-  #In highlander: Select patient, read_depth, patho, chr, pos columns
-  #Filter with patient ZH135614, chr1
-  #Sort by ascending order
-  #
-  #Note discrepancy in results. (e.g., locus 1:14653, or 1:17538)
-  dbWriteTable(concoverage,"patho",patho)
-  
-  
-  #Get chr1 info dor child in trio 1
-  rs<-dbSendQuery(concoverage,"select Locus, Average_Depth_sample, Depth_for_ISDBM387751 from chr1")
-  datacoverage<-fetch(rs, n=-1)
-  
-  rs<-dbSendQuery(concoverage,"select * from patho where patient='ZH135614' and chr=1")
-  datapatho<-fetch(rs, n=-1)
-  
-  rs<-dbSendQuery(concoverage,"select patho.Locus, chr, pos, read_depth, Depth_for_ISDBM387751 from patho,chr1 
-                  where patient='ZH135614' and chr=1 and patho.Locus=chr1.Locus")
-  datamatch<-fetch(rs, n=-1)
-  
-  
-  datamatch<-datamatch[sort(datamatch$pos,index.ret=T)$ix,]
-  datamatch[1:10,]
-  
-  i.diff<-which(abs(datamatch[,4]-datamatch[,5])>10)
-  length(i.diff)
-  datamatch[i.diff[1:5],]
-  
-  #Also, in Highlander, 
-  #select patient chr, pos, num_genes, gene_ensembl, db_snp_id_137, filters
-  # for chr4, pos 54969778
-  #To see inconsistent info
-  
-  #3) Why is there no missing value in coverage file (i.e., at one locus, there should be missing data for some samples) 
-  
-}
