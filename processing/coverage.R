497ba38a3268ba2d45761598a28f52cdb9753c79
yannael <yannael@gmail.com>
2015-09-30 08:11:58 +0200

    Spark call

--- processing/coverage.R
+++ processing/coverage.R
@@ -2,15 +2,17 @@ require(RMySQL)
 require(reshape2)
 require(plyr)
 
-createGenotypeMatrix<-function() {
+createGenotypeMatrix<-function(analysisName) {
   
-  analysisName="ULB_Control_vs_NeuroDev_Damaging_Gene"
+  #analysisName="ULB_Control_vs_NeuroDev_Damaging_Gene"
+  #analysisName="tol_ctrl_rare"
+  #analysisName="control_tol_rare"
   
-  load("analyses.Rdata")
+  load(paste("analyses/",analysisName,".Rdata",sep=""))
   
-  DF<-analyses[[analysisName]]$variantData
+  DF<-analysis$variantData
   DF<-DF[,c('ID','Sample_ID','Gene_Ensembl','Zygosity')]
-  samplesID<-analyses[[analysisName]]$samplesID
+  samplesID<-analysis$samplesID
   
   #Trios
   #samplesID<-read.table('mappingZH_ISDBM_withFMC.txt',stringsAsFactors=F)[,1]
@@ -18,8 +20,13 @@ createGenotypeMatrix<-function() {
   genoMat<-dcast(DF,Gene_Ensembl+ID~Sample_ID,fill=0)
   genoMat<-genoMat[,c(1,2,match(samplesID,colnames(genoMat)))] 
   
-  write.table(file="genoMatDamagingNotOrdered.txt",genoMat,col.names=F,row.names=F,quote=F)
+  write.table(file=paste("analyses/",analysisName,"_genoMat.txt",sep=""),genoMat,col.names=F,row.names=F,quote=F)
   
 }
 
-
+andrea<-function() {
+  
+  genes<-read.table("listGenesAndrea.txt",stringsAsFactors=F)[,1]
+  genesChar<-paste(genes,sep="",collapse=",")
+  
+}
