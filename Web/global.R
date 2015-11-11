03f281cebc7bd80dd886ce93d46b1037ccfc7cc6
yannael <yannael@gmail.com>
2015-11-11 18:20:40 +0100

    Update result format

--- Web/global.R
+++ Web/global.R
@@ -4,8 +4,14 @@ require("RMySQL")
 require("jsonlite")
 require('rpivotTable')
 require('plyr')
+library(RJDBC)
 
 source("../processing/coverage.R")
+
+drv <- JDBC(driverClass = "org.apache.hive.jdbc.HiveDriver",
+            classPath = list.files("/Users/yalb/Downloads/impala-jdbc-0.5-2",pattern="jar$",full.names=T),
+            identifier.quote="`")
+
 
 Sys.setenv(SPARK_HOME="/Users/yalb/spark")
 Sys.setenv(PATH=paste0("/Users/yalb/spark/bin:/Users/yalb/spark/sbin:",Sys.getenv("PATH")))
@@ -57,25 +63,26 @@ loadData<-function(sql,noLimit=F,excludeID=T) {
   
   if (sql!="") {
     sql<-paste0(" where ",sql)
-    sql<-gsub(' +, +',"','",sql)
+    sql<-gsub(' *, *',"','",sql)
   }
-  condb<-dbConnect(RSQLite::SQLite(), paste0("../data.db"))
+  #condb<-dbConnect(RSQLite::SQLite(), paste0("../data.db"))
+  #condb <- dbConnect(drv, "jdbc:hive2://192.168.99.101:21050/;auth=noSasl")
+  condb <- dbConnect(drv, "jdbc:hive2://127.0.0.1:21050/;auth=noSasl")
+  
   #nbrows<-dbGetQuery(condb,paste0("select count(*) from variants ",sql))
-  nbrows<-100000
+  nbrows<-1000
   if (noLimit) limit<-""
-  else limit<-" limit 100000"
+  else limit<-" limit 1000"
   nbRowsExceededWarningMessage<-""
-  if (nbrows>100000) {
+  if (nbrows>1000) {
     nbRowsExceededWarningMessage<-paste0("Query returns ",nbrows," records. First 100000 retrieved.")
   }
   
-  data<-dbGetQuery(condb,paste0("select * from variants ",sql,limit))
+  #data<-dbGetQuery(condb,paste0("select * from variants ",sql,limit))
+  data<-dbGetQuery(condb,paste0("select * from gvr4.variants ",sql,limit))
+  
   dbDisconnect(condb)
-  if (excludeID)
-    results<-list(data=data[,-c(1)],nbRowsExceededWarningMessage=nbRowsExceededWarningMessage)
-  else {
-    results<-list(data=data,nbRowsExceededWarningMessage=nbRowsExceededWarningMessage)
-  }
+  results<-list(data=data,nbRowsExceededWarningMessage=nbRowsExceededWarningMessage)
   results
 }
 
@@ -96,14 +103,11 @@ load("../filtersTypes.Rdata")
 load("../filtersPhenotypesTypes.Rdata")
 
 
-#load("../analyses.Rdata")
-
-
 get4<-function(l) {l[[4]]}
 get1<-function(l) {l[[1]]}
 get2<-function(l) {l[[2]]}
 
-procRes<-function(results,analysis) {
+procRes<-function(results) {
   res<-list()
   res$name<-results[[1]]
   res$scale<-results[[2]]
@@ -113,13 +117,17 @@ procRes<-function(results,analysis) {
   res$run_time<-round(results[[6]],digits=2)
   res$scores<-sapply(results[[7]],get2)
   res$locus<-sapply(results[[7]],get1)
+  res$caseSampleID=results[[8]]
+  res$controlSampleID=results[[9]]
+  res$group1name=results[[10]]
+  res$group2name=results[[11]]
   if (res$scale=="variant") {
     if (res$scope=="monogenic") {
-      variantsID<-res$locus[2,]
-      i.match<-match(variantsID,analysis$variantData$ID)
-      scoreSummary<-analysis$variantData[i.match,c(1,3:6,16:35)]
+      variantsID<-res$locus[1,]
+      scoreSummary<-cbind(t(data.frame(sapply(variantsID,strsplit,':'))),res$locus[2,])
+      colnames(scoreSummary)<-c("Chr","Position","Reference","Alternative",'Gene_Symbol')
+      rownames(scoreSummary)<-NULL
       scoreSummary[,'Gene_Symbol']<-paste0("<a href='http://www.ncbi.nlm.nih.gov/omim/?term=",scoreSummary[,'Gene_Symbol'],"' target='_blank'>",scoreSummary[,'Gene_Symbol'],"</a>")
-      
       #if (results[[2]]=="pairVariantsMonogenic") {
       #  uniqueid2<-apply(res$locus[5:7,],2,paste,collapse=":")
       #  to.keep<-match(uniqueid2,dbvariants$uniqueid)
@@ -146,38 +154,28 @@ procRes<-function(results,analysis) {
   
   if (res$scale=="gene") {
     if (res$scope=="monogenic") {
-      #load(paste("analyses/",analysisName,".Rdata",sep="")
       geneID<-res$locus
-      i.match<-match(geneID,analysis$variantData$Gene_Ensembl)
-      scoreSummary1<-analysis$variantData[i.match,c(17:20)]
-      scoreSummary1[,'Gene_Symbol']<-paste0("<a href='http://www.ncbi.nlm.nih.gov/omim/?term=",scoreSummary1[,'Gene_Symbol'],"' target='_blank'>",scoreSummary1[,'Gene_Symbol'],"</a>")
-      res$scoreSummary<-cbind(t(res$scores),scoreSummary1)
-      colnames(res$scoreSummary)[1:3]<-c("Score","Score_Case","Score_Control")
+      geneID<-paste0("<a href='http://www.ncbi.nlm.nih.gov/omim/?term=",geneID,"' target='_blank'>",geneID,"</a>")
+      res$scoreSummary<-cbind(t(res$scores),geneID)
+      colnames(res$scoreSummary)<-c("Score","Score_Case","Score_Control","Gene_Symbol")
     }
     
     if (res$scope=="digenic") {
       genes<-ldply(res$scores[1,])
       res$scores<-res$scores[2:4,]
-      geneID<-genes[,1]
-      i.match<-match(geneID,analysis$variantData$Gene_Ensembl)
-      scoreSummary1<-analysis$variantData[i.match,c(17:20)]
-      colnames(scoreSummary1)<-paste0(colnames(scoreSummary1),"1")
-      geneID<-genes[,2]
-      i.match<-match(geneID,analysis$variantData$Gene_Ensembl)
-      scoreSummary2<-analysis$variantData[i.match,c(17:20)]
-      colnames(scoreSummary2)<-paste0(colnames(scoreSummary2),"2")
-      scoreSummary1[,'Gene_Symbol1']<-paste0("<a href='http://www.ncbi.nlm.nih.gov/omim/?term=",scoreSummary1[,'Gene_Symbol1'],"' target='_blank'>",scoreSummary1[,'Gene_Symbol1'],"</a>")
-      scoreSummary2[,'Gene_Symbol2']<-paste0("<a href='http://www.ncbi.nlm.nih.gov/omim/?term=",scoreSummary2[,'Gene_Symbol2'],"' target='_blank'>",scoreSummary2[,'Gene_Symbol2'],"</a>")
-      res$scoreSummary<-cbind(t(res$scores),scoreSummary1,scoreSummary2)
-      colnames(res$scoreSummary)[1:3]<-c("Score","Score_Case","Score_Control")
+      geneID1<-genes[,1]
+      geneID1<-paste0("<a href='http://www.ncbi.nlm.nih.gov/omim/?term=",geneID1,"' target='_blank'>",geneID1,"</a>")
+      geneID2<-genes[,2]
+      geneID2<-paste0("<a href='http://www.ncbi.nlm.nih.gov/omim/?term=",geneID2,"' target='_blank'>",geneID2,"</a>")
+      res$scoreSummary<-cbind(t(res$scores),geneID1,geneID2)
+      colnames(res$scoreSummary)<-c("Score","Score_Case","Score_Control","Gene_Symbol1","Gene_Symbol2")
     }
   }
-  
   res
 }
 
-analysesFiles<-dir("analyses/","*.Rdata")
-analysesNames<-as.vector(unlist(sapply(analysesFiles,strsplit,'.Rdata')))
+analysesFiles<-dir("analyses2/","*.*")
+analysesNames<-as.vector(unlist(sapply(analysesFiles,strsplit,'.txt')))
 
 #resultsAll<-list()
 #for (analysisName in analysesNames) {
