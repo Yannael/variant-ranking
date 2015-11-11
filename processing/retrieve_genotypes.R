d80c00f209df94371b708240cae0e5da32898274
yannael <yannael@gmail.com>
2015-11-11 18:18:58 +0100

    For Impala storage

--- processing/retrieve_genotypes.R
+++ processing/retrieve_genotypes.R
@@ -1,17 +1,5 @@
 require(RMySQL)
 
-priorlist<-c("TUBB2A","TCL1A","BRDG1","HTPAP","PPPAPDC1B","IGKV4","IGLL1",
-             "IGKV1D-13","CD798","HS3ST1","SH2D1B","MS4A1","TLR5","FCRL1",
-             "PNOC","SLC8A1","FCRL2",
-             "FAM26F","POU4F2", "SIX4", "ADAMTS7", "GRIN2C","CHPF","HRH3",
-             "ANK3","MUC17","CCDC125","TP73","PHLDA2","RAB6C",
-             "FCGBP","OBSCN","UNC79","WASL","FAM26F","DDX20","NUDT16",
-             "IQSEC2","LOC1002407","CD40L","POP7",
-             "IQSEC2","CD40L","POP7","TDG","PRR23C","MRPS24","NARR","GCGR",
-             "POU4F2","MUC17","ADAMTS7","INTS1","KRT6C",
-             "PKHD1L1","SDHA","MYH3","ETFA","MYO3A","PTPRN2","CHPPF","YPEL5","HRH3","PCIF1")
-
-geneIDs<-c("TUBB2A","TCL1A")
 
 #filters='PASS' and
 #read_depth>10 and
@@ -38,8 +26,6 @@ fieldsInfoNewNames<-c("Sample_ID","Chr","Position","Reference","Alternative",
                       "CADD_Phred","CADD_Raw","VEST_Score","PPH2_hdiv_score", "PPH2_hdiv_pred", 
                       "PPH2_hvar_score", "PPH2_hvar_pred", "SIFT_Score", "SIFT_Pred", "Short_Tandem_Repeat")
 
-
-
 dummy<-function() {
   #To which DB to connect
   
@@ -57,13 +43,6 @@ dummy<-function() {
   
 }
 
-createIndices<-function(dbname,tablename,cnames) {
-  for (cname in cnames) {
-    print(cname)
-    dbGetQuery(dbname,paste("create index ",cname," on ",tablename,"(",cname,")"))
-  }
-}
-
 createCopyHighlanderDB<-function() {
   
   source(connectFile)
@@ -77,7 +56,7 @@ createCopyHighlanderDB<-function() {
   
   #For ULB
   #Takes about 3 minutes
-  #system.time(data <- dbGetQuery(highlanderdb,paste0("select ",fields_select," from ",tablename)))
+  system.time(data <- dbGetQuery(highlanderdb,paste0("select ",fields_select," from ",tablename)))
   uniqueID<-paste(data$chr, data$pos, data$reference, data$alternative, sep=":")
   
   #id.redundant<-tapply(data$gene_symbol,uniqueID,length)
@@ -153,6 +132,95 @@ createCopyHighlanderDB<-function() {
   
 }
 
+createCopyHighlanderDB2<-function() {
+  
+  source(connectFile)
+  
+  samples<-dbGetQuery(highlanderdb,paste0("select distinct patient from ",tablename))[,1]
+  fields_select<-paste(unique(c(fieldsInfo)),collapse=",")
+  
+  #For 1000g
+  subsamples<-paste0("('",paste(samples[1:100],sep="",collapse="','"),"')") #75s
+  system.time(data <- dbGetQuery(highlanderdb,paste0("select ",fields_select," from ",tablename," where patient in ",subsamples)))
+  
+  #For ULB
+  #Takes about 3 minutes
+  system.time(data <- dbGetQuery(highlanderdb,paste0("select ",fields_select," from ",tablename)))
+  uniqueID<-paste(data$chr, data$pos, data$reference, data$alternative, sep=":")
+  
+  #id.redundant<-tapply(data$gene_symbol,uniqueID,length)
+  
+  zygosity<-data$zygosity
+  zygosity[zygosity=="Homozygous"]<-2
+  zygosity[zygosity=="Heterozygous"]<-1
+  data$zygosity<-zygosity
+  data<-cbind(ID=uniqueID,data)
+  
+  #data[is.na(data)]<-''
+  colnames(data)<-c(fieldsInfoNewNames)
+  #data<-data[order(data[,3],data[,4]),]
+  
+  variantsdb <- dbConnect(RSQLite::SQLite(), "variants1000.db")
+  dbWriteTable(variantsdb,"variants",data,overwrite=T,row.names=F)
+  
+  phenotypesdb<-dbConnect(RSQLite::SQLite(), paste0("phenotypes1000.db"))
+  phenotypes<-dbReadTable(phenotypesdb,"phenotypes")
+  dbWriteTable(variantsdb,"phenotypes",phenotypes,overwrite=T,row.names=F)
+  
+  #2 minutes with 15M entries
+  system.time(data<-dbGetQuery(variantsdb,paste0("select * from variants,phenotypes where variants.Sample_ID=phenotypes.Sample_ID")))
+  
+  #Remove duplicate Sample_ID column
+  data<-data[,-which(colnames(data)=="Sample_ID")[2]]
+  
+  
+  for (i in 1:ncol(data)) {
+    if (class(data[,i])=="character") data[is.na(data[,i]),i]<-''
+  }
+  data[is.na(data)]<-'\\N'
+  
+  data[,'Chr']<-factor(data[,'Chr'],levels=c(1:22,'X','Y'))
+  data[,'Change_Type']<-as.factor(data[,'Change_Type'])
+  data[,'Snpeff_Effect']<-as.factor(data[,'Snpeff_Effect'])
+  data[,'Snpeff_Impact']<-as.factor(data[,'Snpeff_Impact'])
+  data[,'SIFT_Pred']<-as.factor(data[,'SIFT_Pred'])
+  data[,'PPH2_hvar_pred']<-as.factor(data[,'PPH2_hvar_pred'])
+  data[,'PPH2_hdiv_pred']<-as.factor(data[,'PPH2_hdiv_pred'])
+  data[,'Consensus_MAF']<-as.numeric(data[,'Consensus_MAF'])
+  data[,'Consensus_MAC']<-as.numeric(data[,'Consensus_MAC'])
+  data[,'CADD_Phred']<-as.numeric(data[,'CADD_Phred'])
+  data[,'CADD_Raw']<-as.numeric(data[,'CADD_Raw'])
+  data[,'VEST_Score']<-as.numeric(data[,'VEST_Score'])
+  data[,'PPH2_hdiv_score']<-as.numeric(data[,'PPH2_hdiv_score'])
+  data[,'PPH2_hvar_score']<-as.numeric(data[,'PPH2_hvar_score'])
+  data[,'SIFT_Score']<-as.numeric(data[,'SIFT_Score'])
+  data[,'Allelic_Depth_Ref_Prop']<-as.numeric(data[,'Allelic_Depth_Ref_Prop'])
+  data[,'Allelic_Depth_Alt_Prop']<-as.numeric(data[,'Allelic_Depth_Alt_Prop'])
+  data[,'Genotype_Quality']<-as.numeric(data[,'Genotype_Quality'])
+  data[,'Mapping_Quality_Zero_Reads']<-as.numeric(data[,'Mapping_Quality_Zero_Reads'])
+  data[,'Filters']<-as.factor(data[,'Filters'])
+  
+  data[,'Data_Source']<-as.factor(data[,'Data_Source'])
+  data[,'Pathology']<-as.factor(data[,'Pathology'])
+  data[,'Gender']<-as.factor(data[,'Gender'])
+  data[,'Super_Population']<-as.factor(data[,'Super_Population'])
+  data[,'Population']<-as.factor(data[,'Population'])
+  
+  filtersTypes<-getFiltersFromTable(data[,-1])
+  save(file="filtersTypes.Rdata",filtersTypes)
+  
+  datadb<-dbConnect(RSQLite::SQLite(), paste0("data1000.db"))
+  dbWriteTable(datadb,"variants",data,row.names=F,overwrite=T)
+  dbDisconnect(datadb)
+  write.table(file="variantsULB.csv",data,col.names=F,row.names=F,sep=",",quote=F)
+  
+  missingGenes<-datawhich(data[,'Gene_Ensembl']=='')
+  ID<-data[missingGenes,'ID']
+  subData<-data[which(data[,'ID'] %in% ID),c('ID','Gene_Ensembl')]
+  tt<-tapply(subData[,1],subData[,1],length)
+  
+}
+
 countLevels<-function(vec) {
   length(unique(vec))
 }
@@ -170,6 +238,12 @@ dummy3<-function() {
   system.time(data<-dbGetQuery(condb,paste0("select count(*) from variants"))) #1,5 - 6M rows
   system.time(data<-dbGetQuery(condb,paste0("select * from variants"))) #45s
   system.time(write.table(file="variantsULB.csv",data,col.names=F,row.names=F,sep=",",quote=F)) #102s, 1,39GB
+  
+}
+
+writeForImpala<-function() {
+  data3<-data[1:100000,]
+  write.table(file="data3.csv",data3,col.names=F,row.names=F,quote=F,sep=",")
   
 }
 
